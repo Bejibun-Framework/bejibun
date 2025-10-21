9d2f1c61450fb06f8358ff978a530ac99f47d158
ghulje <ghulam@jejeharapan.com>
2025-10-21 18:27:02 +0700

    feat: adding maintenance modes from @bejibun/core

--- bootstrap.ts
+++ bootstrap.ts
@@ -1,1 +1,6 @@
+/**
+ * DO NOT REMOVE THIS LINE
+ *
+ * REMOVING THIS WILL MAKE YOUR APP CRASHED!!!
+ */
 import "@bejibun/core/bootstrap";
--- server.ts
+++ server.ts
@@ -1,1 +1,6 @@
+/**
+ * DO NOT REMOVE THIS LINE
+ *
+ * REMOVING THIS WILL MAKE YOUR APP WON'T RUN!!!
+ */
 import "@bejibun/core/server";
