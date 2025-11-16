196574a7fc1193c62e6758aa5bd6c704b4aa9383
Hafiizh Ghulam <hafiizh.ghulam@gmail.com>
2025-11-17 01:26:56 +0700

    chore: update packages

---
+++ config/x402.ts
@@ -0,0 +1,10 @@
+const config = {
+    version: 1,
+    network: "solana-devnet",
+    address: "GAnoyvy9p3QFyxikWDh9hA3fmSk2uiPLNWyQ579cckMn",
+    price: "$0.01",
+    timeout: 60,
+    forceJson: false,
+    testnet: true
+};
+export default config;
