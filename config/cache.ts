45b75c9d5fef17a1b1d63e218e36ad8759082d0f
Havea Crenata <havea.crenata@gmail.com>
2025-12-14 21:09:10 +0700

    feat: update config/cache.ts based on @bejibun/cache v0.1.15

--- config/cache.ts
+++ config/cache.ts
@@ -1,14 +1,17 @@
 import App from "@bejibun/app";
+import CacheDriverEnum from "@/enums/CacheDriverEnum";
 
 const config: Record<string, any> = {
     connection: "local",
 
     connections: {
         local: {
+            driver: CacheDriverEnum.Local,
             path: App.Path.storagePath("cache") // absolute path
         },
 
         redis: {
+            driver: CacheDriverEnum.Redis,
             host: Bun.env.REDIS_HOST,
             port: Bun.env.REDIS_PORT,
             password: Bun.env.REDIS_PASSWORD,
