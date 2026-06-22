443cd76a2b037598918af76ff64f5d469718656a
Havea Crenata <havea.crenata@gmail.com>
2026-06-22 13:45:55 +0700

    feat(x402): added new x402 config

--- config/x402.ts
+++ config/x402.ts
@@ -1,10 +1,14 @@
-const config = {
-    version: 1,
-    network: "solana-devnet",
-    address: "GAnoyvy9p3QFyxikWDh9hA3fmSk2uiPLNWyQ579cckMn",
+import type {TScheme} from "@bejibun/x402/types/x402";
+
+const config: Record<string, any> = {
+    version: 2,
+    scheme: "exact" as TScheme,
     price: "$0.01",
-    timeout: 60,
-    forceJson: false,
-    testnet: true
+    network: "eip155:84532",
+    payTo: "0xdABe8750061410D35cE52EB2a418c8cB004788B3",
+    facilitator: {
+        url: "https://x402.org/facilitator"
+    }
 };
+
 export default config;
