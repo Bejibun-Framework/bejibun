c5c4c269b8e62028ccc46f55192d09108691261a
Hafiizh Ghulam <hafiizh.ghulam@gmail.com>
2026-06-26 08:01:30 +0700

    feat: update config/x402.ts

--- config/x402.ts
+++ config/x402.ts
@@ -1,14 +1,21 @@
-import type {TScheme} from "@bejibun/x402/types/x402";
+import type {Network, TScheme} from "@bejibun/x402/types/x402";
+import {facilitator} from "@coinbase/x402";
 
 const config: Record<string, any> = {
     version: 2,
     scheme: "exact" as TScheme,
-    price: "$0.01",
-    network: "eip155:84532",
-    payTo: "0xdABe8750061410D35cE52EB2a418c8cB004788B3",
-    facilitator: {
-        url: "https://x402.org/facilitator"
-    }
+    price: "$0.0001",
+    networks: {
+        evm: {
+            network: "eip155:8453" as Network,
+            payTo: "0xdABe8750061410D35cE52EB2a418c8cB004788B3"
+        },
+        svm: {
+            network: "solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp" as Network,
+            payTo: "GAnoyvy9p3QFyxikWDh9hA3fmSk2uiPLNWyQ579cckMn"
+        }
+    },
+    facilitator: facilitator
 };
 
 export default config;
